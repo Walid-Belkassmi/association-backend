3c883bc75e2738ecb06db44cd9467bb8bdbc2a8f
Walid <belkassmi.walid@gmail.com>
2022-08-22 20:34:21 +0200

    back done

--- middlewares/associations.js
+++ middlewares/associations.js
@@ -1,19 +1,14 @@
 const associations = require("../asociations");
 
-const checkIfExists = (req, res, next) => {
+const verifyAssociation = (req, res, next) => {
   const { slug } = req.params;
 
   const association = associations.find((association) => {
     return slug === association.slug;
   });
 
-  const associationIndex = associations.findIndex((association) => {
-    return slug === association.slug;
-  });
-
   if (association) {
     req.association = association;
-    req.associationIndex = associationIndex;
     next();
   } else {
     res.status(404).json("Association not found");
@@ -21,5 +16,5 @@ const checkIfExists = (req, res, next) => {
 };
 
 module.exports = {
-  checkIfExists,
+  verifyAssociation,
 };
--- routes/associations.js
+++ routes/associations.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const associations = require("../asociations");
-const { checkIfExists } = require("../middlewares/associations");
+const { verifyAssociation } = require("../middlewares/associations");
+const messages = require("../massages.json");
+const moment = require("moment");
 
 // Affiche les 3 associations
 app.get("/", (req, res) => {
@@ -9,8 +11,26 @@ app.get("/", (req, res) => {
 });
 
 // Affiche 1 association
-app.get("/:slug", checkIfExists, (req, res) => {
+app.get("/:slug", verifyAssociation, (req, res) => {
   res.json(req.association);
+});
+
+// Affiche les messages/commentaires
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+// Permet de poster un message/commentaire
+app.post("/", verifyAssociation, (req, res) => {
+  const message = {
+    name: req.body.name,
+    message: req.body.message,
+    time: moment().format("MMMM Do YYYY, h:mm:ss a"),
+    slug: req.body.slug,
+  };
+
+  messages.unshift(message);
+  res.status(201).json("Message sent");
 });
 
 module.exports = app;
